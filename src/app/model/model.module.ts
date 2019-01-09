a647c283e09b49058d862216e5d856afcab5d22b
fabietto82 <jolfsquad@gmail.com>
2019-01-09 19:41:09 +0100

    first commit... npm instal required to run the app...

---
+++ src/app/model/model.module.ts
@@ -0,0 +1,12 @@
+import {ProductRepository} from './product.repository';
+import {StaticDataSource} from './static.datasource';
+import {NgModule} from '@angular/core';
+import {Cart} from './cart.model';
+
+@NgModule({
+  providers: [ProductRepository, StaticDataSource , Cart]
+})
+
+export class ModelModule {
+
+}
